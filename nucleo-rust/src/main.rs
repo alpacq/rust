6f44f3bfc6f27a68c66669e7775a992f6d6344ab
Krzysztof <krzysieklam@outlook.com>
2023-05-26 11:13:12 +0200

    interrupt USART added

--- nucleo-rust/src/main.rs
+++ nucleo-rust/src/main.rs
@@ -5,16 +5,14 @@
 //! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
 //! the reference manual for an explanation. This is not an issue on the blue pill.
 
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
 use panic_halt as _;
 
-use nb::block;
-
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{pac::{self}, prelude::*, serial::{self, Config, Serial, StopBits}};
+use stm32f1xx_hal::{pac::{self, interrupt, USART2}, prelude::*, serial::{Config, Serial, StopBits, Tx, Rx}};
 use core::fmt::Write;
 use heapless::Vec;
 
@@ -43,54 +41,71 @@ impl Command {
         to.cmd = self.cmd;
         to.args = Vec::new();
         for i in 0..self.args.len() {
-            to.args[i] = self.args[i];
+            to.args.push(self.args[i]).unwrap();
         }
     }
 }
 
-// static mut RX: Option<Rx<pac::USART2>> = None;
-// static mut TX: Option<Tx<pac::USART2>> = None;
+static mut RX: Option<Rx<USART2>> = None;
+static mut TX: Option<Tx<USART2>> = None;
+static mut CURRENT_COMMAND: Command = Command { len: 1, cmd: 0, args: Vec::new() };
+static mut RX_STATE: RxState = RxState::Length;
 
-fn receive_command<RX>(serial_rx: &mut RX, cmd: &mut Command) -> usize
-    where RX: embedded_hal::serial::Read<u8, Error = serial::Error>
-{
-    let mut rx_phase = RxState::Length;
-
-    loop {
-        // Read the word that was just sent. Blocks until the read is complete.
-        let received = block!(serial_rx.read()).unwrap();
-
-        match rx_phase {
-            RxState::Length => {
-                if received >= 48 && received <= 57 {
-                    let cmd_length = received - 48;
-                    if cmd_length == 0 {
-                        return 0;
-                    }
-
-                    rx_phase = RxState::Data {
-                        command: Command::new(cmd_length as usize),
-                        idx: 0,
-                    };
-                } else {
-                    rx_phase = RxState::Length;
-                }
-            }
-
-            RxState::Data { ref mut command, ref mut idx } => {
-                if *idx == 0 {
-                    command.cmd = received as u8;
-                } else {
-                    command.args.push(received as u8).unwrap();
-                }
-                *idx += 1;
-                if *idx == command.len {
-                    command.copy(cmd);
-                    return command.len;
-                }
-            }
+unsafe fn uart_command_response(command: &Command) {
+    if let Some(tx) = TX.as_mut() {
+        writeln!(tx, "Length of cmd is {}\r", command.len).unwrap();
+        writeln!(tx, "Command code is {}\r", command.cmd).unwrap();
+        for i in 0..command.args.len() {
+            writeln!(tx, "Argument {} is {}\r", i, command.args[i]).unwrap();
         }
     }
+}
+
+#[interrupt]
+unsafe fn USART2() {
+    cortex_m::interrupt::free(|_| {
+        if let Some(rx) = RX.as_mut() {
+            while rx.is_rx_not_empty() {
+                if let Ok(received) = nb::block!(rx.read()) {
+                    match RX_STATE {
+                        RxState::Length => {
+                            if received >= 48 && received <= 57 {
+                                let cmd_length = received - 48;
+                                if cmd_length == 0 {
+                                    return;
+                                }
+
+                                RX_STATE = RxState::Data {
+                                    command: Command::new(cmd_length as usize),
+                                    idx: 0,
+                                };
+                            } else {
+                                RX_STATE = RxState::Length;
+                            }
+                        }
+
+                        RxState::Data { ref mut command, ref mut idx } => {
+                            if *idx == 0 {
+                                command.cmd = received as u8;
+                            } else {
+                                command.args.push(received as u8).unwrap();
+                            }
+                            *idx += 1;
+                            if *idx == command.len {
+                                command.copy(&mut CURRENT_COMMAND);
+                                RX_STATE = RxState::Length;
+                                uart_command_response(&CURRENT_COMMAND);
+                            }
+                        }
+                    }
+                }
+                rx.listen_idle();
+            }
+            if rx.is_idle() {
+                rx.unlisten_idle();
+            }
+        }
+    })
 }
 
 #[entry]
@@ -122,52 +137,21 @@ fn main() -> ! {
         &clocks
     );
 
-    writeln!(serial.tx, "Please type characters to echo:\r\n").unwrap();
+    serial.tx.listen();
+    serial.rx.listen();
+    serial.rx.listen_idle();
 
-    let mut current_command = Command::new(1);
+    writeln!(serial.tx, "Please type command |len||cmd||args..|:\r\n").unwrap();
 
-    /* Endless loop */
+    cortex_m::interrupt::free(|_| unsafe {
+        TX.replace(serial.tx);
+        RX.replace(serial.rx);
+    });
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(pac::Interrupt::USART2);
+    }
+
     loop {
-        // Receive message from master device.
-        let received_msg_len = receive_command(&mut serial.rx, &mut current_command);
-        for i in 0..received_msg_len - 1 {
-            writeln!(serial.tx, "Inputted character is {}\r\n", current_command.args[i]).unwrap();
-        }
+        cortex_m::asm::wfi()
     }
 }
-
-// LED blinking
-// #[entry]
-// fn main() -> ! {
-//     // Get access to the core peripherals from the cortex-m crate
-//     let cp = cortex_m::Peripherals::take().unwrap();
-//     // Get access to the device specific peripherals from the peripheral access crate
-//     let dp = pac::Peripherals::take().unwrap();
-//
-//     // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-//     // HAL structs
-//     let mut flash = dp.FLASH.constrain();
-//     let rcc = dp.RCC.constrain();
-//
-//     // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-//     // `clocks`
-//     let clocks = rcc.cfgr.freeze(&mut flash.acr);
-//
-//     // Acquire the GPIOA peripheral
-//     let mut gpioa = dp.GPIOA.split();
-//
-//     // Configure gpio A pin 5 as a push-pull output. The `crh` register is passed to the function
-//     // in order to configure the port. For pins 0-7, crl should be passed instead.
-//     let mut led = gpioa.pa5.into_push_pull_output(&mut gpioa.crl);
-//     // Configure the syst timer to trigger an update every second
-//     let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
-//     timer.start(1.Hz()).unwrap();
-//
-//     // Wait for the timer to trigger an update and change the state of the LED
-//     loop {
-//         block!(timer.wait()).unwrap();
-//         led.set_high();
-//         block!(timer.wait()).unwrap();
-//         led.set_low();
-//     }
-// }
